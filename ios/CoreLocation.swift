71f459184b62648358766946adfbe9a0afc2c0f7
davit.darsalia <darsalia.david1998@gmail.com>
2024-07-30 14:29:25 +0400

    fix(core): default multiply method removed

--- ios/CoreLocation.swift
+++ ios/CoreLocation.swift
@@ -4,43 +4,38 @@ import React
 
 @objc(CoreLocation)
 class CoreLocation: RCTEventEmitter, LocationManagerDelegate {
-    
+
     override init() {
         super.init()
         LocationManager.shared.delegate = self
     }
-    
+
     override static func requiresMainQueueSetup() -> Bool {
         return true
     }
-    
+
     @objc
     func requestWhenInUseAuthorization() {
         LocationManager.shared.requestWhenInUseAuthorization()
     }
-    
+
     @objc
     func startUpdatingLocation() {
         LocationManager.shared.startUpdatingLocation()
     }
-    
+
     @objc
     func stopUpdatingLocation() {
         LocationManager.shared.stopUpdatingLocation()
     }
-    
-    @objc(multiply:withB:withResolver:withRejecter:)
-    func multiply(a: Float, b: Float, resolve:RCTPromiseResolveBlock,reject:RCTPromiseRejectBlock) -> Void {
-        resolve(a * b)
-    }
-    
+
     func didUpdateLocation(_ location: CLLocation) {
         sendEvent(withName: "onLocationUpdate", body: [
             "latitude": location.coordinate.latitude,
             "longitude": location.coordinate.longitude
         ])
     }
-    
+
     override func supportedEvents() -> [String]! {
         return ["onLocationUpdate"]
     }
